a3d9d7a031fd7c62928ec927e1ac6fd49d8b6787
TJ Klimoski <tjklimoski@gmail.com>
2024-02-21 23:10:09 -0500

    Handle get and post request on /register view in auth bp

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -16,3 +16,36 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+
+# The views for auth blueprint:
+# views handle the logic for routes
+@bp.route("/register", methods=("GET", "POST"))
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+
+        if not username:
+            error = "Username is required"
+        elif not password:
+            error = "Password if required"
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUE (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    # handles the GET request:
+    return render_template("auth/register.html")
