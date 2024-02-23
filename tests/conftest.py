3e67defcb59872c5f32055d94611213672ecad41
TJ Klimoski <tjklimoski@gmail.com>
2024-02-22 23:59:52 -0500

    define pytest fixtures, and intalize app for testing

---
+++ tests/conftest.py
@@ -0,0 +1,45 @@
+import os
+import tempfile
+
+import pytest
+
+# This import is possible because we made the app into a lib
+from flaskr import create_app
+from flaskr.db import get_db, init_db
+
+# Open data.sql in the tests directory as read-binary, and store its content in _data_sql
+with open(os.path.join(os.path.dirname(__file__), "data.sql"), "rb") as f:
+    _data_sql = f.read().decode("utf8")
+
+
+@pytest.fixture
+def app():
+    db_fd, db_path = tempfile.mkstemp()
+
+    app = create_app(
+        {
+            "TESTING": True,
+            "DATABASE": db_path,
+        }
+    )
+
+    with app.app_context():
+        init_db()
+        get_db().executescript(_data_sql)
+
+    # yield is like a return statement, but instead of terminating the execution of the function, yield only pauses it's execution.
+    yield app
+
+    # after the test is over - remove all temp files:
+    os.close(db_fd)
+    os.unlink(db_path)
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
