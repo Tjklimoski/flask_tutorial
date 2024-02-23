9af8b01ea32ef09788b40894393111e8fb8d8f0f
TJ Klimoski <tjklimoski@gmail.com>
2024-02-22 22:10:02 -0500

    Modify update endpoint to take PATCH method in place of POST

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -71,12 +71,12 @@ def get_post(id, check_author=True):
     return post
 
 
-@bp.route("/<int:id>/update", methods=("GET", "POST"))
+@bp.route("/<int:id>/update", methods=("GET", "PATCH"))
 @login_required
 def update(id):
     post = get_post(id)
 
-    if request.method == "POST":
+    if request.method == "PATCH":
         title = request.form["title"]
         body = request.form["body"]
         error = None
