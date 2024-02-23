c384c7bb0996ac6f19a5953ac8a9bbb798b6a331
TJ Klimoski <tjklimoski@gmail.com>
2024-02-22 21:35:30 -0500

    Fix syntax error

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -48,31 +48,37 @@ def create():
     # GET / error return value
     return render_template("blog/create.html")
 
-def get_post(id, check_author=True)
-    post = get_db().execute(
-        "SELECT p.id, title, body, created, author_id, username"
-        " FROM post p JOIN user u ON p.author_id = u.id"
-        " WHERE p.id = ?",
-        (id,)
-    ).fetchone()
+
+def get_post(id, check_author=True):
+    post = (
+        get_db()
+        .execute(
+            "SELECT p.id, title, body, created, author_id, username"
+            " FROM post p JOIN user u ON p.author_id = u.id"
+            " WHERE p.id = ?",
+            (id,),
+        )
+        .fetchone()
+    )
 
     if post is None:
         abort(404, f"Post id {id} doesn't exist.")
-    
+
     # Check if the user fetching this post is the author. only run if check_author is True
-    if check_author and post['author_id'] != g.user['id']:
+    if check_author and post["author_id"] != g.user["id"]:
         abort(403)
-    
+
     return post
 
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+
+@bp.route("/<int:id>/update", methods=("GET", "POST"))
 @login_required
 def update(id):
     post = get_post(id)
 
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
+    if request.method == "POST":
+        title = request.form["title"]
+        body = request.form["body"]
         error = None
 
         if not title:
@@ -85,11 +91,9 @@ def update(id):
         else:
             db = get_db()
             db.execute(
-                "UPDATE post SET title = ?, body = ?"
-                " WHERE id = ?",
-                (title, body, id)
+                "UPDATE post SET title = ?, body = ?" " WHERE id = ?", (title, body, id)
             )
             db.commit()
-            return redirect(url_for('blog.index'))
-        
-    return render_template('blog/update.html', post=post)
+            return redirect(url_for("blog.index"))
+
+    return render_template("blog/update.html", post=post)
